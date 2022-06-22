bcfa36c2e929f2e7f48714abec551243930b5458
tmp <zhang_hongning@aliyun.com>
2022-06-22 15:41:45 +0800

    Wed Jun 22 15:41:44     2022

---
+++ live2d/chtholly/L2Dwidget.init.js
@@ -0,0 +1,24 @@
+L2Dwidget.init({
+	"pluginRootPath": "js/chtholly/",
+	"pluginJsPath": "js/chtholly/lib/",
+	"pluginModelPath": "js/chtholly/assets/",
+	"tagMode": false,
+	"log": false,
+	"model": {
+		"scale": 1,
+		"jsonPath": "js/chtholly/assets/chtholly.model.json"
+	},
+	"display": {
+		"position": "right",
+		"width": 200,
+		"height": 400,
+		"hOffset": -30,
+		"vOffset": -60
+	},
+	"mobile": {
+		"show": true
+	},
+	"react": {
+		"opacity": 1.0
+	}
+});
